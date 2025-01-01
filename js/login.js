fba293a5f63a08d3c0362e12586e300c900f6467
shad-bin-abi-aydid <shadaydid@gmail.com>
2025-01-01 19:40:43 +0000

    Try to fixed

--- js/login.js
+++ js/login.js
@@ -5,7 +5,6 @@ document.getElementById('button-login').addEventListener('click', function (even
     const pinNumber = document.getElementById('pin-number').value;
     
     if(phoneNumber === '07341009647' && pinNumber === 'Aydid@'){
-        console.log('You are logged in');
         window.location.href = '/home.html';
     }
     else{
